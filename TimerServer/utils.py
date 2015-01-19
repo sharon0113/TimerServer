f8002ef4729e7104611722e36067f77a833d84d7
root <root@iZ237rrqqmjZ.(none)>
2015-01-19 17:27:42 +0800

    modified:   TimerServer/utils.py

--- TimerServer/utils.py
+++ TimerServer/utils.py
@@ -1,6 +1,6 @@
 PORT = "http://121.41.85.39/"
 ROOT = "/mnt/m3u8/"
-INTERSECTION = 60
+INTERSECTION = 30
 M3U8PATH = "/mnt/m3u8live/m3u8/"
 M3U8SUBPATH = "/mnt/m3u8live/m3u8Sub/"
 M3U8NEWPATH = "/mnt/m3u8live/m3u8New/"
